aa76b84cbd0859ca8aa0b3c22356d113b222b03e
VeryGoodDev <very_good_dev@outlook.com>
2021-03-27 03:20:14 -0600

    Make preload show raw stats data

--- preload.js
+++ preload.js
@@ -1,3 +1,31 @@
-window.addEventListener(`DOMContentLoaded`, () => {
-  document.querySelector(`.dynamic`).textContent = `This was dynamically added with JS!`
+const path = require(`path`)
+const fs = require(`fs`).promises
+
+const MC_PATH = path.resolve(`/Users/Kenobi/AppData/Roaming/.minecraft`)
+const MC_SAVES_PATH = path.join(MC_PATH, `/saves`)
+
+window.addEventListener(`DOMContentLoaded`, async () => {
+  const container = document.querySelector(`.dynamic`)
+  const worlds = await fs.readdir(MC_SAVES_PATH)
+  const statsFilePaths = worlds.map((worldName) => path.join(MC_SAVES_PATH, worldName, `/stats`))
+  const [firstPath] = statsFilePaths
+  const [statsFileName] = await fs.readdir(firstPath)
+  const statsFilePath = path.join(firstPath, statsFileName)
+  const statsContentsRaw = await fs.readFile(statsFilePath)
+  const { stats } = JSON.parse(statsContentsRaw)
+  Object.keys(stats).forEach((key) => {
+    const div = document.createElement(`div`)
+    div.innerHTML = `<h2>${key}</h2>`
+    Object.keys(stats[key])
+      .sort((a, b) => {
+        return stats[key][b] - stats[key][a]
+      })
+      .forEach((subkey) => {
+        const stat = document.createElement(`div`)
+        stat.textContent = `${key} - ${subkey}: ${stats[key][subkey]}`
+        div.appendChild(stat)
+      })
+    container.appendChild(div)
+  })
+  container.innerHTML += `sup`
 })
