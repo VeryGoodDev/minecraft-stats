cc984e6fd48ee5ef4d30ca33053f1b7c9b5ed8bc
VeryGoodDev <very_good_dev@outlook.com>
2021-03-28 02:57:04 -0600

    Playing with esbuild a bit

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef {import("esbuild").BuildOptions} BuildOptions
+ */
+
+const [env] = process.argv.slice(2)
+/**
+ * @type {BuildOptions}
+ */
+const baseConfig = {
+  entryPoints: [`src/index.tsx`],
+  outdir: `build`,
+  bundle: true,
+  format: `esm`,
+  target: [`chrome89`],
+}
+/**
+ * @type {BuildOptions}
+ */
+const devConfig = {
+  ...baseConfig,
+  sourcemap: true,
+}
+/**
+ * @type {BuildOptions}
+ */
+const prodConfig = {
+  ...baseConfig,
+  minify: true,
+}
+require(`esbuild`).buildSync(env === `--prod` ? prodConfig : devConfig)
