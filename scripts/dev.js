8c3cb88568607341dcac374d89cb7154bc70ca13
VeryGoodDev <very_good_dev@outlook.com>
2021-04-10 02:40:45 -0600

    Add build complete message to dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,7 +1,11 @@
 const { exec } = require(`child_process`)
+const { performance } = require(`perf_hooks`)
 const runBuild = require(`./esbuild`)
 
+const buildStart = performance.now()
 runBuild().then(() => {
+  const buildTime = (performance.now() - buildStart).toFixed(4)
+  console.log(`Finished all builds in ${buildTime} milliseconds. Starting electron now`)
   exec(`yarn start:electron`, { windowsHide: true }, (error) => {
     if (error) {
       console.error(`Error running \`yarn start:electron\`:`, error)
